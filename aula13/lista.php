b24e16613fcb92b87e3bbe32e39f46c97e75f8ae
=marcelo soares <mbsoaresinfor@gmail.com>
2022-05-27 21:05:47 -0300

    aula 13

---
+++ aula13/lista.php
@@ -0,0 +1,8 @@
+<?php
+
+for($i=0; $i < 5; $i++ ){
+    echo "<a href=pagina.php?id=".$i.">link</a></br>";
+}
+
+
+?>
---
+++ aula13/processaCalculadora.php
@@ -0,0 +1,9 @@
+<?php
+
+$valor1= $_POST["v1"];
+$valor2= $_POST["v2"];
+$soma = $valor1 + $valor2;
+
+echo "O resultado da soma é: " . $soma;
+
+?>
